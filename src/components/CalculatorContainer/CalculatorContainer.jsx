23873b1b1d9590a18e1aab4a15112230fcc24ec9
Jared Kroke <jared.kroke@gmail.com>
2023-04-23 16:48:47 -0500

    Created CalculatorContainer component and css file.  Moving on to general styling before creating buttons.

---
+++ src/components/CalculatorContainer/CalculatorContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './CalculatorContainer.css';
+
+function CalculatorContainer() {
+    return (
+        <div className='Container'>
+            
+        </div>
+    );
+}
