f2e732bfecbd773ebe296c1f6e0a6efecaeb6e7d
Jared Kroke <jared.kroke@gmail.com>
2023-04-26 22:05:33 -0500

    Added buttons and basic styling for them.  Going to create toggle and clear history buttons.

---
+++ src/components/CalculatorButtons/CalculatorButtons.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './CalculatorButtons.css';
+
+function CalculatorButtons() {
+    return (
+        <div className='CalcButtons'>
+            <div className='Row1'>
+                <button id='clearBtn'>C</button>
+                <button id='negationBtn'>+/-</button>
+                <button id='percentBtn'>%</button>
+                <button id='divisionBtn'>/</button>
+            </div>
+            <div className='Row2'>
+                <button id='seven'>7</button>
+                <button id='eight'>8</button>
+                <button id='nine'>9</button>
+                <button id='multiBtn'>*</button>
+            </div>
+            <div className='Row3'>
+                <button id='four'>4</button>
+                <button id='five'>5</button>
+                <button id='six'>6</button>
+                <button id='subtractBtn'>-</button>
+            </div>
+            <div className='Row4'>
+                <button id='one'>1</button>
+                <button id='two'>2</button>
+                <button id='three'>3</button>
+                <button id='addBtn'>+</button>
+            </div>
+            <div className='Row5'>
+                <button id='zero'>0</button>
+                <button id='decimal'>.</button>
+                <button id='equalSign'>=</button>
+            </div>
+        </div>
+    );
+}
+
+
+export default CalculatorButtons;
--- src/components/CalculatorContainer/CalculatorContainer.jsx
+++ src/components/CalculatorContainer/CalculatorContainer.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './CalculatorContainer.css';
 import CalculatorDisplay from '../CalculatorDisplay/CalculatorDisplay.jsx';
+import CalculatorButtons from '../CalculatorButtons/CalculatorButtons';
 
 function CalculatorContainer() {
     return (
         <div className='Container'>
             <CalculatorDisplay />
+            <CalculatorButtons />
         </div>
     );
 }
