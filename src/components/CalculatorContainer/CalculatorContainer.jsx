fbd0d5f0a880a73c36eccead15f02fbf307f500b
Jared Kroke <jared.kroke@gmail.com>
2023-04-26 21:22:32 -0500

    Created and added basic styling to calculator display window.  Moving on to buttons.

--- src/components/CalculatorContainer/CalculatorContainer.jsx
+++ src/components/CalculatorContainer/CalculatorContainer.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './CalculatorContainer.css';
+import CalculatorDisplay from '../CalculatorDisplay/CalculatorDisplay.jsx';
 
 function CalculatorContainer() {
     return (
         <div className='Container'>
-            
+            <CalculatorDisplay />
         </div>
     );
 }
