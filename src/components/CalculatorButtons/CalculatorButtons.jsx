d1ebf7b956b3a77197fecd5acfe9dccdc15360f1
Jared Kroke <jared.kroke@gmail.com>
2023-04-30 15:28:15 -0500

    Added most of the functionality for the calculator itself. Need to work on getting the decimal, percentage, and negation buttons to work properly.

--- src/components/CalculatorButtons/CalculatorButtons.jsx
+++ src/components/CalculatorButtons/CalculatorButtons.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import './CalculatorButtons.css';
+import CalculatorDisplay from '../CalculatorDisplay/CalculatorDisplay';
 
 function CalculatorButtons() {
 
-    useEffect(() => {
-    
-    }, []);
+
 
     function calculate(calculation) {
         if (typeof num1 === 'number' && typeof num2 === 'number' && calculation === '=') {
@@ -19,22 +18,25 @@ function CalculatorButtons() {
             } else if (operation === '/') {
                 setResult(num1 / num2);
             }
+            setOperation('');
+            setIsFirstNumClicked(false);
         } else if (calculation === 'C') {
             setResult('');
             setIsFirstNumClicked(false);
+            setNum1('');
+            setNum2('');
         } else if (calculation === '%') {
-            if (isFirstNumClicked) {
-                setNum2(num2 * 0.01);
-            } else {
-                setNum1(num1 * 0.01);
-            }
+            setResult(num1 * 0.01);
         } else {
-            if (isFirstNumClicked) {
-                setNum2(num2 * -1);
-            } else {
-                setNum1(num1 * -1);
-            }
+            setResult(num1 * -1);
         }
+
+        if (!result) {
+            setNum1('');
+            setNum2('');
+            setIsFirstNumClicked(false);
+        }
+
         if (result !== '') {
             // will complete the POST to back-end here
         }
@@ -48,47 +50,58 @@ function CalculatorButtons() {
 
     function pickNum(val) {
         if (!isFirstNumClicked && (typeof val === 'number' || val === '.')) {
-            setNum1(Number(num1 + '' + val));
-            setIsFirstNumClicked(true);
-        } else if (isFirstNumClicked && (typeof val === 'number' || val === '.')) {
-            setNum2(Number(num2 + '' + val));
-            setIsFirstNumClicked(false);
+            if (operation !== '') {
+                setNum2(Number(num2 + '' + val));
+                setIsFirstNumClicked(true);
+                if (result) {
+                    setNum1(result);
+                    calculate('=');
+                }
+            } else {
+                setNum1((Number(num1 + '' + val)))
+            }
+            setResult('');
+        } else if (isFirstNumClicked && !result && (typeof val === 'number' || val === '.')) {
+                setNum2(Number(num2 + '' + val));
         }
     }
 
 
     return (
-        <div className='CalcButtons' id='containerChild'>
-            <div className='Row1'>
-                <div id='inner'><button id='clearBtn' onClick={calculate('C')}>C</button></div>
-                <div id='inner'><button id='negationBtn' onClick={calculate('--')}>+/-</button></div>
-                <div id='inner'><button id='percentBtn' onClick={calculate('%')}>%</button></div>
-                <div id='inner'><button id='divisionBtn' onClick={'/'}>/</button></div>
+        <>
+            <CalculatorDisplay num1={num1} num2={num2} result={result} isFirstNumClicked={isFirstNumClicked} />
+            <div className='CalcButtons' id='containerChild'>
+                <div className='Row1'>
+                    <div id='inner'><button id='clearBtn' onClick={() => calculate('C')}>C</button></div>
+                    <div id='inner'><button id='negationBtn' onClick={() => calculate('--')}>+/-</button></div>
+                    <div id='inner'><button id='percentBtn' onClick={() => calculate('%')}>%</button></div>
+                    <div id='inner'><button id='divisionBtn' onClick={() => setOperation('/')}>/</button></div>
+                </div>
+                <div className='Row2'>
+                    <button id='seven' onClick={() => pickNum(7)}>7</button>
+                    <button id='eight' onClick={() => pickNum(8)}>8</button>
+                    <button id='nine' onClick={() => pickNum(9)}>9</button>
+                    <button id='multiBtn' onClick={() => setOperation('*')}>*</button>
+                </div>
+                <div className='Row3'>
+                    <button id='four' onClick={() => pickNum(4)}>4</button>
+                    <button id='five' onClick={() => pickNum(5)}>5</button>
+                    <button id='six' onClick={() => pickNum(6)}>6</button>
+                    <button id='subtractBtn' onClick={() => setOperation('-')}>-</button>
+                </div>
+                <div className='Row4'>
+                    <button id='one' onClick={() => pickNum(1)}>1</button>
+                    <button id='two' onClick={() => pickNum(2)}>2</button>
+                    <button id='three' onClick={() => pickNum(3)}>3</button>
+                    <button id='addBtn' onClick={() => setOperation('+')}>+</button>
+                </div>
+                <div className='Row5'>
+                    <button id='zero' onClick={() => pickNum(0)}>0</button>
+                    <button id='decimal' onClick={() => pickNum('.')}>.</button>
+                    <button id='equalSign' onClick={() => calculate('=')}>=</button>
+                </div>
             </div>
-            <div className='Row2'>
-                <button id='seven' onClick={pickNum(7)}>7</button>
-                <button id='eight' onClick={pickNum(8)}>8</button>
-                <button id='nine' onClick={pickNum(9)}>9</button>
-                <button id='multiBtn' onClick={'*'}>*</button>
-            </div>
-            <div className='Row3'>
-                <button id='four' onClick={pickNum(4)}>4</button>
-                <button id='five' onClick={pickNum(5)}>5</button>
-                <button id='six' onClick={pickNum(6)}>6</button>
-                <button id='subtractBtn' onClick={setOperation('-')}>-</button>
-            </div>
-            <div className='Row4'>
-                <button id='one' onClick={pickNum(1)}>1</button>
-                <button id='two' onClick={pickNum(2)}>2</button>
-                <button id='three' onClick={pickNum(3)}>3</button>
-                <button id='addBtn' onClick={setOperation('+')}>+</button>
-            </div>
-            <div className='Row5'>
-                <button id='zero' onClick={pickNum(0)}>0</button>
-                <button id='decimal' onClick={pickNum('.')}>.</button>
-                <button id='equalSign' onClick={calculate('=')}>=</button>
-            </div>
-        </div>
+        </>
     );
 }
 
--- src/components/CalculatorDisplay/CalculatorDisplay.jsx
+++ src/components/CalculatorDisplay/CalculatorDisplay.jsx
@@ -2,10 +2,13 @@ import React from 'react';
 import './CalculatorDisplay.css';
 
 
-function CalculatorDisplay() {
+function CalculatorDisplay({num1, num2, result, isFirstNumClicked}) {
     return (
         <div className='CalculatorDisplay'>
-            <h1 id='displayText'>1234567890</h1>
+            <h1 id='displayText'>{result ? result : 
+                (isFirstNumClicked ? (num2 ? num2 : 0) 
+                : (num1 ? num1 : 0))}
+            </h1>
         </div>
     );
 }
