4cad5a0e31f014fbef84e51fcb2be9836f37d72e
Jared Kroke <jared.kroke@gmail.com>
2023-05-07 18:56:18 -0500

    Refactored code to include the manipulation of only a single unit of local state, rather than several. Going to continue with this new strategy and test in the browser next.

--- src/components/CalculatorButtons/CalculatorButtons.jsx
+++ src/components/CalculatorButtons/CalculatorButtons.jsx
@@ -6,115 +6,118 @@ import CalculatorDisplay from '../CalculatorDisplay/CalculatorDisplay';
 function CalculatorButtons() {
 
 
-
     function calculate(calculation) {
-        let tempNum1 = Number(num1);
-        let tempNum2 = Number(num2);
-        if (typeof tempNum1 === 'number' && typeof tempNum2 === 'number' && calculation === '=') {
-            if (operation === '+') {
-                setResult(tempNum1 + tempNum2);
-            } else if (operation === '-') {
-                setResult(tempNum1 - tempNum2);
-            } else if (operation === '*') {
-                setResult(tempNum1 * tempNum2);
-            } else if (operation === '/') {
-                setResult(tempNum1 / tempNum2);
-            }
-            setOperation('');
-            setIsFirstNumClicked(false);
-        } else if (calculation === 'C') {
-            setResult('');
-            setIsFirstNumClicked(false);
-            setNum1('');
-            setNum2('');
-        } else if (calculation === '%') {
-            if (tempNum1 === 0) {
-                setResult(result * 0.01);
+        let num1;
+        let num2;
+        if (calculation === '=') {
+            if (expression.indexOf('+') !== expression.lastIndexOf('+')
+            || expression.indexOf('-') !== expression.lastIndexOf('-')
+            || expression.indexOf('/') !== expression.lastIndexOf('/')
+            || expression.indexOf('*') !== expression.lastIndexOf('*') ||
+            expression.includes('+') && expression.includes('-') ||
+            expression.includes('+') && expression.includes('*') ||
+            expression.includes('+') && expression.includes('/') ||
+            expression.includes('-') && expression.includes('*') ||
+            expression.includes('-') && expression.includes('/') ||
+            expression.includes('*') && expression.includes('/')) {
+                alert('You can only perform one operation at a time!');
+                return;
+            } else if (expression.includes('+')) {
+                num1 = expression.slice(0, expression.indexOf('+'));
+                if (expression.indexOf('+') === expression.length - 1 || 
+                expression.indexOf('+') === 0) {
+                    alert('You must choose two separate numbers to complete the operation!');
+                    return;
+                } 
+                num2 = expression.slice(expression.indexOf('+') + 1);
+                setExpression(Number(num1) + Number(num2) + '');
+            } else if (expression.includes('-')) {
+                num1 = expression.slice(0, expression.indexOf('-'));
+                if (expression.indexOf('-') === expression.length - 1 ||
+                expression.indexOf('-') === 0) {
+                    alert('You must choose two separate numbers to complete the operation!');
+                    return;
+                } 
+                num2 = expression.slice(expression.indexOf('-') + 1);
+                setExpression(Number(num1) - Number(num2) + '');
+            } else if (expression.includes('*')) {
+                num1 = expression.slice(0, expression.indexOf('*'));
+                if (expression.indexOf('*') === expression.length - 1 ||
+                expression.indexOf('*') === 0) {
+                    alert('You must choose two separate numbers to complete the operation!');
+                    return;
+                } 
+                num2 = expression.slice(expression.indexOf('*') + 1);
+                setExpression(Number(num1) * Number(num2) + '');
+            } else if (expression.includes('/')) {
+                num1 = expression.slice(0, expression.indexOf('/'));
+                if (expression.indexOf('/') === expression.length - 1 ||
+                expression.indexOf('/') === 0) {
+                    alert('You must choose two separate numbers to complete the operation!');
+                    return;
+                } 
+                num2 = expression.slice(expression.indexOf('/') + 1);
+                setExpression(Number(num1) / Number(num2) + '');
             } else {
-                setResult(tempNum1 * 0.01);
+                alert('You must choose an operation for calculation to occur!');
+            }
+        } else if (calculation === 'C') {
+            setExpression('');
+        } else if (calculation === '--') {
+            if (Number(expression) === NaN) {
+                alert('You can only use this operation on a number, not an expression!');
+                return;
+            } else {
+                setExpression(Number(expression) * -1 + '');
             }
         } else {
-            if (tempNum1 === 0) {
-                setResult(result * -1);
+            if (Number(expression) === NaN) {
+                alert('You can only use this operation on a number, not an expression!');
+                return;
             } else {
-                setResult(tempNum1 * -1);
+                setExpression(Number(expression) * 0.01 + '');
             }
-        }
-
-        if (!result) {
-            setNum1('');
-            setNum2('');
-            setIsFirstNumClicked(false);
-        }
-
-        if (result !== '') {
-            // will complete the POST to back-end here
         }
     }
 
-    const [operation, setOperation] = useState('');
-    const [num1, setNum1] = useState('');
-    const [num2, setNum2] = useState('');
-    const [result, setResult] = useState('');
-    const [isFirstNumClicked, setIsFirstNumClicked] = useState(false);
+    const [expression, setExpression] = useState('');
 
-    function pickNum(val) {
-        if (!isFirstNumClicked && (typeof val === 'number' || val === '.')) {
-            if (operation !== '') {
-                setNum2(num2 + '' + val);
-                setIsFirstNumClicked(true);
-                if (result) {
-                    setNum1(result);
-                    calculate('=');
-                }
-            } else {
-                setNum1(num1 + '' + val)
-            }
-            setResult('');
-        } else if (isFirstNumClicked && !result && (typeof val === 'number' || val === '.')) {
-            setNum2(num2 + '' + val);
-        } else {
-            setNum1(result);
-            if (num2.length === result.length) {
-                setNum2(val);
-            } else {
-                setNum2(num2 + '' + val);
-            }
-        }
+    function addChar(val) {
+        setExpression(expression + '' + val);
     }
 
 
     return (
         <>
-            <CalculatorDisplay num1={num1} num2={num2} result={result} isFirstNumClicked={isFirstNumClicked} />
+            <CalculatorDisplay expression={expression} />
             <div className='CalcButtons' id='containerChild'>
                 <div className='Row1'>
                     <div id='inner'><button id='clearBtn' onClick={() => calculate('C')}>C</button></div>
                     <div id='inner'><button id='negationBtn' onClick={() => calculate('--')}>+/-</button></div>
                     <div id='inner'><button id='percentBtn' onClick={() => calculate('%')}>%</button></div>
-                    <div id='inner'><button id='divisionBtn' onClick={() => setOperation('/')}>/</button></div>
+                    <div id='inner'><button id='divisionBtn' onClick={() => addChar('/')}>/</button></div>
                 </div>
                 <div className='Row2'>
-                    <button id='seven' onClick={() => pickNum(7)}>7</button>
-                    <button id='eight' onClick={() => pickNum(8)}>8</button>
-                    <button id='nine' onClick={() => pickNum(9)}>9</button>
-                    <button id='multiBtn' onClick={() => setOperation('*')}>*</button>
+                    <button id='seven' onClick={() => addChar(7)}>7</button>
+                    <button id='eight' onClick={() => addChar(8)}>8</button>
+                    <button id='nine' onClick={() => addChar(9)}>9</button>
+                    <button id='multiBtn' onClick={() => addChar('*')}>*</button>
                 </div>
                 <div className='Row3'>
-                    <button id='four' onClick={() => pickNum(4)}>4</button>
-                    <button id='five' onClick={() => pickNum(5)}>5</button>
-                    <button id='six' onClick={() => pickNum(6)}>6</button>
-                    <button id='subtractBtn' onClick={() => setOperation('-')}>-</button>
+                    <button id='four' onClick={() => addChar(4)}>4</button>
+                    <button id='five' onClick={() => addChar(5)}>5</button>
+                    <button id='six' onClick={() => addChar(6)}>6</button>
+                    <button id='subtractBtn' onClick={() => addChar('-')}>-</button>
                 </div>
                 <div className='Row4'>
-                    <button id='one' onClick={() => pickNum(1)}>1</button>
-                    <button id='two' onClick={() => pickNum(2)}>2</button>
-                    <button id='three' onClick={() => pickNum(3)}>3</button>
-                    <button id='addBtn' onClick={() => setOperation('+')}>+</button>
+                    <button id='one' onClick={() => addChar(1)}>1</button>
+                    <button id='two' onClick={() => addChar(2)}>2</button>
+                    <button id='three' onClick={() => addChar(3)}>3</button>
+                    <button id='addBtn' onClick={() => addChar('+')}>+</button>
                 </div>
                 <div className='Row5'>
-                    <button id='zero' onClick={() => pickNum(0)}>0</button>
-                    <button id='decimal' onClick={() => pickNum('.')}>.</button>
+                    <button id='zero' onClick={() => addChar(0)}>0</button>
+                    <button id='decimal' onClick={() => addChar('.')}>.</button>
                     <button id='equalSign' onClick={() => calculate('=')}>=</button>
                 </div>
             </div>
--- src/components/CalculatorDisplay/CalculatorDisplay.jsx
+++ src/components/CalculatorDisplay/CalculatorDisplay.jsx
@@ -2,13 +2,10 @@ import React from 'react';
 import './CalculatorDisplay.css';
 
 
-function CalculatorDisplay({num1, num2, result, isFirstNumClicked}) {
+function CalculatorDisplay({expression}) {
     return (
         <div className='CalculatorDisplay'>
-            <h1 id='displayText'>{result ? result : 
-                (isFirstNumClicked ? (num2 ? num2 : 0) 
-                : (num1 ? num1 : 0))}
-            </h1>
+            <h1 id='displayText'>{expression ? expression : 0}</h1>
         </div>
     );
 }
