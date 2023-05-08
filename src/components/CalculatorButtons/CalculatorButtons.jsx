d8a03f5199081a08fe8e0d04dc953195ea8055aa
Jared Kroke <jared.kroke@gmail.com>
2023-05-08 12:06:05 -0500

    Finished up base functionality of calculator. Have to work on large number formatting in display, then will move on to back-end design.

--- src/components/CalculatorButtons/CalculatorButtons.jsx
+++ src/components/CalculatorButtons/CalculatorButtons.jsx
@@ -6,21 +6,42 @@ import CalculatorDisplay from '../CalculatorDisplay/CalculatorDisplay';
 function CalculatorButtons() {
 
 
-    function calculate(calculation) {
-        let num1;
-        let num2;
-        if (calculation === '=') {
-            if (expression.indexOf('+') !== expression.lastIndexOf('+')
-            || expression.indexOf('-') !== expression.lastIndexOf('-')
-            || expression.indexOf('/') !== expression.lastIndexOf('/')
-            || expression.indexOf('*') !== expression.lastIndexOf('*') ||
-            expression.includes('+') && expression.includes('-') ||
-            expression.includes('+') && expression.includes('*') ||
+    // This function determines which of the four basic operations
+    // is desired.  To start out, it checks to make sure there is only operation 
+    // being performed in the expression.  Then, it makes sure there are two numbers
+    // and performs either addition, subtraction, multiplication, and division, depending
+    // on what is desired.
+    function equals(num1, num2) {
+        let expressionArr = expression.split('');
+        let minusCount = 0;
+        for (let char of expressionArr) {
+            if (char === '-') {
+                minusCount++;
+            }
+        }
+        
+        if (minusCount === 1 && expression.indexOf('-') !== 0 
+        && (expression.includes('+') || 
+        expression.includes('*') || expression.includes('/'))) {
+            alert('You can only perform one operation at a time!');
+            clear();
+            return;
+        } else if (minusCount === 2 && (expression.indexOf('-') !== 0 ||
+        (expression.includes('+') || 
+        expression.includes('*') || expression.includes('/')))) {
+            alert('The operation you are trying to perform is invalid!');
+            clear();
+            return;
+        } else if (minusCount > 2) {
+            alert('You must input a valid expression!');
+            clear();
+            return;
+        }
+        if (expression.includes('+') && expression.includes('*') ||
             expression.includes('+') && expression.includes('/') ||
-            expression.includes('-') && expression.includes('*') ||
-            expression.includes('-') && expression.includes('/') ||
             expression.includes('*') && expression.includes('/')) {
                 alert('You can only perform one operation at a time!');
+                clear();
                 return;
             } else if (expression.includes('+')) {
                 num1 = expression.slice(0, expression.indexOf('+'));
@@ -30,16 +51,13 @@ function CalculatorButtons() {
                     return;
                 } 
                 num2 = expression.slice(expression.indexOf('+') + 1);
-                setExpression(Number(num1) + Number(num2) + '');
-            } else if (expression.includes('-')) {
-                num1 = expression.slice(0, expression.indexOf('-'));
-                if (expression.indexOf('-') === expression.length - 1 ||
-                expression.indexOf('-') === 0) {
-                    alert('You must choose two separate numbers to complete the operation!');
+                if (num1.indexOf('.') !== num1.lastIndexOf('.') || 
+                num2.indexOf('.') !== num2.lastIndexOf('.')) {
+                    alert('Each number must have a maximum of one decimal point to be valid!');
+                    clear();
                     return;
-                } 
-                num2 = expression.slice(expression.indexOf('-') + 1);
-                setExpression(Number(num1) - Number(num2) + '');
+                }
+                setExpression(Number(num1) + Number(num2) + '');
             } else if (expression.includes('*')) {
                 num1 = expression.slice(0, expression.indexOf('*'));
                 if (expression.indexOf('*') === expression.length - 1 ||
@@ -48,6 +66,12 @@ function CalculatorButtons() {
                     return;
                 } 
                 num2 = expression.slice(expression.indexOf('*') + 1);
+                if (num1.indexOf('.') !== num1.lastIndexOf('.') || 
+                num2.indexOf('.') !== num2.lastIndexOf('.')) {
+                    alert('Each number must have a maximum of one decimal point to be valid!');
+                    clear();
+                    return;
+                }
                 setExpression(Number(num1) * Number(num2) + '');
             } else if (expression.includes('/')) {
                 num1 = expression.slice(0, expression.indexOf('/'));
@@ -57,33 +81,88 @@ function CalculatorButtons() {
                     return;
                 } 
                 num2 = expression.slice(expression.indexOf('/') + 1);
+                if (num1.indexOf('.') !== num1.lastIndexOf('.') || 
+                num2.indexOf('.') !== num2.lastIndexOf('.')) {
+                    alert('Each number must have a maximum of one decimal point to be valid!');
+                    clear();
+                    return;
+                }
                 setExpression(Number(num1) / Number(num2) + '');
+            } else if (expression.includes('-')) {
+                num1 = expression.slice(0, expression.lastIndexOf('-'));
+                if (expression.indexOf('-') === expression.length - 1 ||
+                    expression.lastIndexOf('-') === 0) {
+                        alert('You must choose two separate numbers to complete the operation!');
+                        return;
+                } 
+                    num2 = expression.slice(expression.lastIndexOf('-') + 1);
+                    if (num1.indexOf('.') !== num1.lastIndexOf('.') || 
+                    num2.indexOf('.') !== num2.lastIndexOf('.')) {
+                        alert('Each number must have a maximum of one decimal point to be valid!');
+                        clear();
+                        return;
+                    }
+                    setExpression(Number(num1) - Number(num2) + '');
             } else {
                 alert('You must choose an operation for calculation to occur!');
             }
-        } else if (calculation === 'C') {
-            setExpression('');
-        } else if (calculation === '--') {
-            if (Number(expression) === NaN) {
-                alert('You can only use this operation on a number, not an expression!');
-                return;
-            } else {
-                setExpression(Number(expression) * -1 + '');
-            }
+    }
+    
+    // Clears the current expression.
+    function clear() {
+        setExpression('');
+    }
+
+    // Changes the sign of the given number.
+    function negate() {
+        if (isNaN(expression)) {
+            alert('The input is not valid for this operation!');
+            return;
         } else {
-            if (Number(expression) === NaN) {
-                alert('You can only use this operation on a number, not an expression!');
-                return;
-            } else {
-                setExpression(Number(expression) * 0.01 + '');
-            }
+            setExpression(Number(expression) * -1 + '');
+        }
+    }
+
+    // Transforms the selected number into 1 / 100 of itself.
+    function percent() {
+        if (isNaN(expression)) {
+            alert('The input is not valid for this operation!');
+            return;
+        } else {
+            setExpression(Number(expression) * 0.01 + '');
+        }
+    }
+
+    // This function determines what operation to perform on the number(s). It then calls the 
+    // corresponding function.
+    function calculate(calculation) {
+        let num1;
+        let num2;
+        if (calculation === '=') {
+            equals(num1, num2);
+        } else if (calculation === 'C') {
+            clear();
+        } else if (calculation === '--') {
+            negate();
+        } else {
+           percent();
         }
     }
 
     const [expression, setExpression] = useState('');
 
+    /* This function is responsible for the addition of numbers, decimal points,
+    and operation symbols, which are then shown on the calculator display.*/
     function addChar(val) {
-        setExpression(expression + '' + val);
+        if (typeof val !== 'number' && val !== '.' 
+        && val !== '-' && expression.includes(val)) {
+            alert('You can only perform one operation at a time!');
+            return;
+        } else if (expression.length === 1 && expression[0] === '0') {
+            setExpression(val + '');
+        } else {
+            setExpression(expression + '' + val);
+        }
     }
 
 
